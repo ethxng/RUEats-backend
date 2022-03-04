540b37f78e30c23e15f3a96da29e33aef89017c6
Likhit Krishnam <likhit.krishnam2003@gmail.com>
2022-03-04 13:47:11 -0500

    Fixed Error

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require("express");
+const path = require('path');
+const app = express();
+const PORT = 2003;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Welcome to RUEats');
+  });
+
+//app.get('/food', (req, res, next) => {
+//    res.sendFile(path.join(__dirname, '/sample_data.json'));
+//  });
+  
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
